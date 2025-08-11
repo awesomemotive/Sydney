6a3d110134a91a06a3a5d457d1a35b37eeef27d0
vlad706 <vlad@athemes.com>
2025-08-11 10:44:19 +0300

    add upgrade link function

--- inc/extras.php
+++ inc/extras.php
@@ -8,6 +8,32 @@
  */
 
 /**
+ * Build an upgrade/upsell URL with UTM parameters and allow filtering per placement.
+ *
+ * @param string $url  Base URL for the upgrade link (e.g., https://athemes.com/sydney-upgrade).
+ * @param array  $args Key/value query arguments to append (e.g., utm_* params). Optional.
+ * @param string $type Link placement/type identifier (e.g., 'customizer-upgrade-link'). Optional.
+ *
+ * @return string Final upgrade link URL.
+ */
+if ( ! function_exists( 'sydney_admin_upgrade_link' ) ) {
+    function sydney_admin_upgrade_link( $url, $args = array(), $type = 'upgrade-link' ) {
+        if ( ! empty( $args ) ) {
+            $url = add_query_arg( $args, $url );
+        }
+
+        /**
+         * Filter: Modify generated upgrade link before output.
+         *
+         * @param string $url  The generated upgrade link URL.
+         * @param string $type The link placement/type identifier.
+         * @param array  $args The arguments used to build the link.
+         */
+        return apply_filters( 'sydney_upgrade_link', $url, $type, $args );
+    }
+}
+
+/**
  * Adds custom classes to the array of body classes.
  *
  * @param array $classes Classes for the body element.
